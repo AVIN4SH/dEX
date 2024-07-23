266bcfc585e8060f654bd1de32974ff41ddc204f
Avinash <savinashkumarsingh619@gmail.com.com>
2024-07-23 08:53:34 +0530

    2. smoke test for solidity compilation and deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config():
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {},
+    //we can explicitly provide url of hardhat node here, but we don't need to as when we use empty paranthesis, it takes default value i.e; the hardhat node
+  },
 };
